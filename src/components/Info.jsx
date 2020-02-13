612e3915d9990a6f66c4338ddf2cfa9647e25121
Ildar107 <ildar107wk@gmail.com>
2020-02-13 22:34:39 +0300

    feat: update design

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -6,7 +6,7 @@ export default class Info extends React.Component {
             <div className="info">
                 <Infocard name="MSRP" value="$50000"/>
                 <Infocard name="Vehicle name" value="BMW"/>
-                <Infocard name="Monthly payment:" value="$3003"/>
+                <Infocard name="Monthly payment" value="$3003"/>
                 <Infocard name="Taxes" value="$2323"/>
                 <Infocard name="Dealer name" value="Somebody"/>
                 <Infocard name="Dealer phone number" value="8-999-777-82-78"/>
